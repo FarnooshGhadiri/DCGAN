d53ad16c1fd6d06a6d26f0fb33eb71070e8f5b44
Farnoosh Ghadiri <fafar@Farnooshs-MacBook-Pro.local>
2020-03-11 13:59:47 -0400

    last commit

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from spectral_normalization import SpectralNorm
+from IPython.display import HTML
 
 import torchvision.utils as vutils
 from PIL import Image
@@ -34,7 +35,7 @@ ndf = 64
 # num_epochs = 5
 num_epochs = 5
  # learning rate for optimizer
-lr =0.0002
+lr = 0.0002
 
 # Beta Hyper parameter for Adam optimizer
 beta1 = 0.5
@@ -86,20 +87,21 @@ class Generator(nn.Module):
         # state size. (ngf*4) x 8 x 8
         self.de_conv3 = self._make_layer_decode(ngf * 4, ngf * 2, 2,1)
         # state size. (ngf*2) x 16 x 16
-        self.de_conv3 = self._make_layer_decode(ngf*2,ngf,2,1)
+        self.de_conv4 = self._make_layer_decode(ngf*2,ngf,2,1)
         # state size. (ngf) x 32 x 32
-        self.de_conv4 = nn.Sequential(nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),nn.Tanh())
+        self.de_conv5 = nn.Sequential(nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),nn.Tanh())
 
     def forward(self,input):
         feature_1 = self.de_conv1(input)
         feature_2 = self.de_conv2(feature_1)
         feature_3 = self.de_conv3(feature_2)
-        feature_4 = self.de_conv3(feature_3)
-        return feature_4
+        feature_4 = self.de_conv4(feature_3)
+        feature_5 = self.de_conv5(feature_4)
+        return feature_5
 
 
     def _make_layer_decode(self,in_nc,out_nc,strid_nm,Padd_nm):
-        block=[nn.ConvTranspose2d( in_nc, out_nc, 4, strid_nm, Padd_nm, bias=False),
+        block=[nn.ConvTranspose2d(in_nc, out_nc, 4, strid_nm, Padd_nm, bias=False),
                nn.BatchNorm2d(out_nc),
                nn.ReLU(True)]
         return nn.Sequential(*block)
@@ -165,7 +167,7 @@ class Discriminator(nn.Module):
                 nn.LeakyReLU(True)]
         return nn.Sequential(*block)
 
-NetD = Discriminator(ngpu).to(device)
+NetD = Discriminator(ngpu,'BatchNorm').to(device)
 if (device.type=='cuda') and (ngpu>0):
     NetD = nn.DataParallel(NetD,list(range(ngpu)))
 
@@ -178,7 +180,7 @@ print(NetD)
 criterion = nn.BCELoss()
 # Create a batch of noise
 
-fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+fixed_noise = torch.randn(4, nz, 1, 1, device=device)
 
 lable_re = 1
 lable_fa = 0
@@ -196,13 +198,13 @@ print("Starting Training.........")
 # For each epoch
 for epoch in range(num_epochs):
    # For each batch in dalaloader
-    for i,data in enumerate(DataLoader,0):
+    for i,data in enumerate(dataloader,0):
         # Update Discriminator
         # Train Discriminator with Real batches
         NetD.zero_grad()
         Real_data_cpu = data[0].to(device)
         batch_size = Real_data_cpu.size(0)
-        label = torch.full((batch_size),lable_re,device=device)
+        label = torch.full((batch_size,),lable_re,device=device)
         output_D = NetD(Real_data_cpu).view(-1)
         #Calculate loss for real data batch
         Err_real_D = criterion(output_D,label)
@@ -233,7 +235,7 @@ for epoch in range(num_epochs):
         Optimizer_G.step()
         #-----------output status--------------
         if i%5==0:
-            print('\Epoc:%d\Num_epoc:%d\Err_D:%.4f\Err_G:%.4f'),(epoch,num_epochs,ErrG.item(),ErrD.item())
+            print('\Epoc: %d \Num_epoc: %d \Err_D: %.4f Err_G: %.4f' %(epoch,num_epochs,ErrG.item(),ErrD.item()))
         G_loss.append(ErrG.item())
         D_Loss.append((ErrD.item()))
         # Check how the generator is doing by saving G's output on fixed_noise
@@ -244,9 +246,28 @@ for epoch in range(num_epochs):
 
         iters += 1
 
+# Plot the results
+plt.figure(figsize=(10,5))
+plt.title("Generator and discriminator loss (TRaining phase)")
+plt.plot(G_loss,label="G")
+plt.plot(D_Loss,label="D")
+plt.xlabel("Iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
 
 
 
+
+#%%capture
+fig = plt.figure(figsize=(2,2))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_G_list]
+plt.show()
+
+ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+
+HTML(ani.to_jshtml())
 
 
 
